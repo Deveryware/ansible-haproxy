9831ff3d36cc3ea7f6f6e48ef85f7a6aad79f2e4
Yoann LE TOUCHE <yoann.le-touche@deveryware.com>
2019-04-17 15:58:40 +0200

    Add tests for default scenario

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,28 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+conf_md5 = '87457a8be8e47b51d03da3154f4a1373'
+conf_sha = '897215c11150201799b8c4d6e1605ab359904bfab85f39e2da2480752b55c3e8'
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_haproxy_is_installed(host):
+    haproxy = host.package('haproxy')
+    assert haproxy.is_installed
+    assert haproxy.version.startswith('1.5')
+
+
+def test_haproxy_config_exist(host):
+    config = host.file('/etc/haproxy/haproxy.cfg')
+    assert config.exists
+
+
+def test_haproxy_default_config_is_valid(host):
+    config = host.file('/etc/haproxy/haproxy.cfg')
+    assert config.md5sum == conf_md5
+    assert config.sha256sum == conf_sha
+
+
+def test_haproxy_is_running_and_enabled(host):
+    haproxy = host.service('haproxy')
+    assert haproxy.is_running
+    assert haproxy.is_enabled
